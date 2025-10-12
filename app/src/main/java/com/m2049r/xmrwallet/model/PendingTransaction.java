4bf70b43e3003e855306f113a86e5507bd65991f
Oyeniyi Dada <neeyeed@gmail.com>
2025-10-12 17:31:26 +0100

    Transaction sending but reporting wrong amount and txid is null

--- app/src/main/java/com/m2049r/xmrwallet/model/PendingTransaction.java
+++ app/src/main/java/com/m2049r/xmrwallet/model/PendingTransaction.java
@@ -1,27 +1,31 @@
 package com.m2049r.xmrwallet.model;
 
-import java.util.List;
-import java.util.ArrayList;
+import lombok.Getter;
+import lombok.Setter;
 
 public class PendingTransaction {
-
-    /* ===== ENUMS ===== */
-
+    
+    public long handle;
+    
+    PendingTransaction(long handle) {
+        this.handle = handle;
+    }
+    
     public enum Status {
         Status_Ok(0),
         Status_Error(1),
         Status_Critical(2);
-
+        
         private final int value;
-
+        
         Status(int value) {
             this.value = value;
         }
-
+        
         public int getValue() {
             return value;
         }
-
+        
         public static Status fromInteger(int x) {
             switch (x) {
                 case 0:
@@ -35,24 +39,24 @@ public class PendingTransaction {
             }
         }
     }
-
+    
     public enum Priority {
         Priority_Default(0),
         Priority_Low(1),
         Priority_Medium(2),
         Priority_High(3),
         Priority_Last(4);
-
+        
         private final int value;
-
+        
         Priority(int value) {
             this.value = value;
         }
-
+        
         public int getValue() {
             return value;
         }
-
+        
         public static Priority fromInteger(int x) {
             switch (x) {
                 case 0:
@@ -70,86 +74,40 @@ public class PendingTransaction {
             }
         }
     }
-
-    /* ===== INSTANCE VARIABLES ===== */
-
-    public long handle;           // C++ pointer to PendingTransaction
-    private Status status = Status.Status_Ok;
-    private String errorString = "";
-
-    private long amount = 0;
-    private long fee = 0;
-    private final List<String> txIds = new ArrayList<>();
-
-    /* ===== CONSTRUCTOR ===== */
-
-    protected PendingTransaction(long handle) {
-        this.handle = handle;
-    }
-
-    /* ===== JNI BINDINGS ===== */
-
-    public native int getStatusNative(long handle);
-
-    public native String getErrorStringNative(long handle);
-
-    public native boolean commitNative(long handle, String filename, boolean overwrite);
-
-    public native long getAmountNative(long handle);
-
-    public native long getFeeNative(long handle);
-
-    public native String[] getTxIdsNative(long handle);
-
-    public native long getTxCountNative(long handle);
-
-    public native void disposeNative(long handle);
     
-    public native String getFirstTxId();
+    public Status getStatus() {
+        return Status.values()[getStatusJ()];
+    }
     
-    public native byte[] getSerializedTransaction();
+    public native int getStatusJ();
     
-    public native int getStatus();    
-
-
-    /* ===== JAVA WRAPPERS ===== */
-
-    public String getErrorString() {
-        return getErrorStringNative(handle);
+    public native String getErrorString();
+    
+    // commit transaction or save to file if filename is provided.
+    public native boolean commit(String filename, boolean overwrite);
+    
+    public native long getAmount();
+    
+    public native long getDust();
+    
+    public native long getFee();
+    
+    public String getFirstTxId() {
+        String id = getFirstTxIdJ();
+        if (id == null)
+            throw new IndexOutOfBoundsException();
+        return id;
     }
-
-    public boolean commit(String filename, boolean overwrite) {
-        return commitNative(handle, filename, overwrite);
-    }
-
-    public long getAmount() {
-        return getAmountNative(handle);
-    }
-
-    public long getFee() {
-        return getFeeNative(handle);
-    }
-
-    public List<String> getTxId() {
-        String[] ids = getTxIdsNative(handle);
-        txIds.clear();
-        if (ids != null) {
-            for (String id : ids) {
-                txIds.add(id);
-            }
-        }
-        return txIds;
-    }
-
-    public long getTxCount() {
-        return getTxCountNative(handle);
-    }
-
-    public void dispose() {
-        if (handle != 0) {
-            disposeNative(handle);
-            handle = 0;
-        }
+    
+    public native String getFirstTxIdJ();
+    
+    public native long getTxCount();
+    
+    @Getter
+    @Setter
+    private long pocketChange;
+    
+    public long getNetAmount() {
+        return getAmount() - pocketChange;
     }
 }
-
